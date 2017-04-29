70493757e12a1aba6c04fa249704b2e11c72c806
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-29 14:29:53 -0500

    Moved sync to beginning

--- ppm_test.py
+++ ppm_test.py
@@ -55,13 +55,7 @@ num_channels = 8
  at minimum (700) then the pulse between frames increases by 8,000 (1,000 for each frame)
  to 11,700 in order to make up the difference and keep the frame size at 20,000
 """
-frame_size = 23000
-"""
-#Generate synchronization pulse.
-pi.wave_add_generic([pulse(pin_mask, 0, 4000)])
-sync_pulse = pi.wave_create()
-pi.wave_send_repeat(sync_pulse)
-"""
+frame_size = 22500
 
 # Generates 3 waves in which all eight channels recieve 1000, 1500, 2000 microsecond pulses
 # respectively (including 300 microsecond delay)
@@ -72,13 +66,15 @@ for pulse_length in [1000, 1500, 2000]:
     # for calculating delay between frames
     total_pulse_length = pulse_length * 8
 
+    pulses += [pulse(pin_mask, 0, frame_size - total_pulse_length - delay)]
+
     for channel in range(8):
         # 300 delay and 700-1,700 pulse
         pulses += [pulse(0, pin_mask, delay), pulse(pin_mask, 0, pulse_length - delay)]
 
 
     # 9th 300 delay and 3,700 (at least) pulse between frames
-    pulses += [pulse(0, pin_mask, delay), pulse(pin_mask, 0, frame_size - total_pulse_length - delay)]
+    pulses += [pulse(0, pin_mask, delay)]
 
     pi.wave_add_generic(pulses)
     
@@ -92,5 +88,8 @@ for pulse_length in [1000, 1500, 2000]:
     pi.wave_delete(newWave)
 
 
-pi.write(OUTPUT_PIN, 0)
+#Bring pin low... FOREVER
+pi.wave_add_generic([pulse(0, pin_mask, 1000)])
+pi.wave_send_using_mode(pi.wave_create(), WAVE_MODE_REPEAT_SYNC)
+
 pi.stop()
