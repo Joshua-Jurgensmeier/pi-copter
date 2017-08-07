b2d52f893a2360b5e230472ab9009e1cc88878a2
Joshua Jurgensmeier <joshjurg777@gmail.com>
2017-08-07 14:35:09 -0500

    Update throttle_test.py

--- Python-test-scripts/throttle_test.py
+++ Python-test-scripts/throttle_test.py
@@ -1,5 +1,35 @@
+#!!!!!!!!! DID YOU FORGET TO START THE PIGPIO PROCESS?!!!!!!!!!
+
 # 'us' stands for microseconds.  Sorry, I ain't got no mu
-#Rev throttle and hold all other channels at neutral
+# Rev throttle and hold all other channels at neutral
+
+#Created using https://github.com/jsa/flystick.git for reference
+
+"""
+channels
+ 1 : Throttle
+ 2 : Roll
+ 3 : Pitch
+ 4 : Yaw
+ 5 : FlightMode
+ 6 : Collective
+ 7 : Accessory0
+ 8 : Accessory1
+
+All times in microseconds.
+
+ 6,200 (at least) sync pulse between frames         700-1,700 value pulse                        
+        v                 8 channels                      v
+    ________   ___   ___   ___   ___   ___   ___   ___   ___  
+   |  sync  |_| 1 |_| 2 |_| 3 |_| 4 |_| 5 |_| 6 |_| 7 |_| 8 |_
+             ^
+          300 delay                   |_____|
+                                         v
+                          channels (value + delay) 1,000 - 2,000
+|_______________________________________________________________|
+                            v
+                       22,500 frame
+"""
 
 from time import sleep
 from pigpio import *
@@ -14,7 +44,7 @@ pi.wave_clear()
 delay_us = 300
 num_channels = 8
 frame_size_us = 22500
-channel_vals_us = [1875, 1500, 1500, 1500, 1500, 1500, 1500, 1500]
+channel_vals_us = [1700, 1200, 1200, 1200, 1200, 1200, 1200, 1200]
 total_length_us = 0
 pulses = []
 
@@ -22,22 +52,23 @@ pulses = []
 #assemble frame
 for channel in range(num_channels):
     
-    #add the length of both pulses to total_length for calculating sync pulse
+    #add the length of value and delay pulses to total_length for calculating sync pulse
     total_length_us += delay_us + channel_vals_us[channel]
 
     #add delay
     pulses += [pulse(0, pin_mask, delay_us)]
+    
     #add channel
     pulses += [pulse(pin_mask, 0, channel_vals_us[channel])]
 
-#add last delay
+#add last (9th) delay
+total_length += delay_us
 pulses += [pulse(0, pin_mask, delay_us)]
 
 #add sync pulse at beginning of frame
-pulses.insert(0, pulse(pin_mask, 0, frame_size - total_length - delay))
+pulses.insert(0, pulse(pin_mask, 0, frame_size_us - total_length_us))
 
-#generate frame
-
+#create frame from data
 pi.wave_add_generic(pulses)    
 newWave = pi.wave_create() 
 pi.wave_send_using_mode(newWave, WAVE_MODE_REPEAT_SYNC)
